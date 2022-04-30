f6a9bafaeb35a11970b7be5e08b37728390aeba6
bruse <Bruse@ro.ru>
2022-04-30 12:36:55 +0300

    Maket2

---
+++ app/src/main/java/com/example/lesson1maket2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lesson1maket2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
